54a7a8ff07b0e4c7f1fb3aa34740744efd0e2d62
Pedro Aulicino <96741195+PedroAulicinoC@users.noreply.github.com>
2026-02-10 23:07:56 -0300

    Test for user/admin login and logout validation

--- cypress/e2e/cnarios/role-based-login-flow.cy.js
+++ cypress/e2e/cnarios/role-based-login-flow.cy.js
@@ -11,6 +11,22 @@ describe('Role-Based Login Flow Challenge', () => {
 
     it('Verify error message on invalid credentials', () => {
         cy.loginCnarios('4dmin', 'admin1234');
-        cy.contains('Invalid username or password.').should('be.visible')
+        cy.contains('Invalid username or password.').should('be.visible');
+    });
+
+    it('Login as user and validate user dashboard', () => {
+        cy.loginCnarios('user', 'user123');
+        cy.contains('User Dashboard').should('be.visible');
+    });
+
+    it('Login as admin and validate admin dashboard', () => {
+        cy.loginCnarios('admin', 'admin123');
+        cy.contains('Admin Dashboard').should('be.visible');
+    });
+
+    it('Verify logout functionality resets the session', () => {
+        cy.loginCnarios('admin', 'admin123');
+        cy.get('.MuiCardContent-root > .MuiButtonBase-root').click();
+        cy.contains('Login').should('be.visible');
     });
 })
